4fdce1bb4f82bce39755e908c4ce1440cf06d5d9
BraedenChubak <112727144+BraedenChubak@users.noreply.github.com>
2024-10-10 15:04:38 +0000

    10/10

--- MSOE2012_9.cpp
+++ MSOE2012_9.cpp
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <math.h>
 #include <iostream>
+#include <bits/stdc++.h>
+#include <vector>
 using namespace std;
 
 int main() {
@@ -8,20 +10,52 @@ int main() {
     cout << "Enter Roman Numeral: ";
     cin >> roman;
 
+    char romanArr[roman.length()+1];
+    strcpy(romanArr, roman.c_str());
+
     int total = 0;
-    int curIndex = 0;
+
     // I, V, X, L, C, D, M
-    string numerals[7];
-    numerals[0] = "I";
-    numerals[1] = "V";
-    numerals[2] = "X";
-    numerals[3] = "L";
-    numerals[4] = "C";
-    numerals[5] = "D";
-    numerals[6] = "M";
-    for (int i = 0; i < 7; i++) {
-        for (int j = 0; j < roman.length(); j++) {
-            
+    for (int i = 0; i < roman.length(); i++) {
+        switch (romanArr[i]) {
+            case 'I':
+                if (romanArr[i+1] == 'V' || romanArr[i+1] == 'X') {
+                    total--;
+                } else {
+                    total++;
+                }
+                break;
+            case 'V':
+                total += 5;
+                break;
+            case 'X':
+                if (romanArr[i+1] == 'L' || romanArr[i+1] == 'C') {
+                    total -= 10;
+                } else {
+                    total += 10;
+                }
+                break;
+            case 'L':
+                total += 50;
+                break;
+            case 'C':
+                if (romanArr[i+1] == 'D' || romanArr[i+1] == 'M') {
+                    total -= 100;
+                } else {
+                    total += 100;
+                }
+                break;
+            case 'D':
+                total += 500;
+                break;
+            case 'M':
+                total += 1000;
+                break;
         }
     }
-}
+    cout << "The number is " << total;
+
+/*
+Enter Roman Numeral: MCDXLIV
+The number is 1444
+*/
