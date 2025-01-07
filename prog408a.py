bbb12dd53955941f5ff72d9d8a92fbafd70de47e
BraedenChubak <112727144+BraedenChubak@users.noreply.github.com>
2025-01-07 16:07:37 +0000

    1/7

---
+++ prog408a.py
@@ -0,0 +1,21 @@
+# Import time, your sorting library, and your helper class
+
+def main():
+    try:
+        data = []
+        with open("data/prg408a.dat", 'r') as f:
+            lines = f.readlines()
+            for line in lines:
+                id, score = line.split(' ')
+                id = int(id)
+                score = int(score)
+                # make helper class objects and add to data
+        # TODO: your code here
+        ...
+    except Exception as e:
+        print("Error: ", e)
+
+    pass
+
+if __name__ == "__main__":
+    main()
---
+++ prog408b.py
@@ -0,0 +1,16 @@
+# import time and searching library
+
+def main():
+    try:
+        data = []
+        with open("data/prog408b.dat", 'r') as f:
+            data = [int(line) for line in f]
+        # TODO: your code here
+        ...
+    except Exception as e:
+        print("Error: ", e)
+
+    pass
+
+if __name__ == "__main__":
+    main()
