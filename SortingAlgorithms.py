6056ca8615024a40e07392b369eb16579df87ba7
BraedenChubak <112727144+BraedenChubak@users.noreply.github.com>
2025-01-28 16:49:55 +0000

    1/28

--- SortingAlgorithms.py
+++ SortingAlgorithms.py
@@ -22,4 +22,67 @@ class SortingAlgorithms:
                 arr[j+1] = arr[j]
                 j -= 1
             arr[j+1] = key
+
+    @staticmethod
+    def quicksort(A, low=0, high=None):
+        if high is None:
+            high = len(A)-1
+        if low < high:
+            pivot = SortingAlgorithms.partition(A, low, high)
+            SortingAlgorithms.sort(A, low, pivot-1)
+            SortingAlgorithms.sort(A, pivot+1, high)
+        return A
+    
+    @staticmethod
+    def partition(A, low, high):
+        pivot = A[high]
+        i = low-1
+        for j in range(low, high):
+            if A[j] <= pivot:
+                i += 1
+                SortingAlgorithms.swap(A, i, j)
+        SortingAlgorithms.swap(A, i+1, high)
+        return i+1
+
+    @staticmethod
+    def swap(A, i, j):
+        temp = A[i]
+        A[i] = A[j]
+        A[j] = temp
+        # A[i], A[j] = A[j], A[i]
+
+    @staticmethod
+    def merge_sort(arr, l=0, r=None):
+        if r is None:
+            r = arr.length
+        if left < right:
+            mid = (int)((left + right) / 2)
+            merge_sort(arr, left, mid)
+            merge_sort(arr, mid + 1, right)
+            merge(arr, left, mid, right)
+
+    @staticmethod
+    def merge(arr, l, m, r):
+        i = 0
+        j = 0
+        k = 0
+        finalarr = [0] * arr.length
+        while i < m - l + 1 and j < r - m:
+            if arr[i] < arr[m + j]:
+                finalarr[k] = arr[i]
+                i += 1
+            else:
+                finalarr[k] = arr[m + j]
+                j += 1
+            k += 1
+        while i < m - l + 1:
+            finalarr[k] = arr[i]
+            i += 1
+            k += 1
+        while j < r - m:
+            finalarr[k] = arr[m + j]
+            j += 1
+            k += 1
+        return finalarr
+
         
